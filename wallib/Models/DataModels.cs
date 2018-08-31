aafb5f3cc2937bb8145dbced990b37fb2e43b415
Kevin Weidner <weidnerk@gmail.com>
2018-08-31 16:37:41 -0400

    Initial connect.

---
+++ wallib/Models/DataModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallib.Models
+{
+    [Table("WalItems")]
+    public class WalItem
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string DetailUrl { get; set; }
+        public int CategoryID { get; set; }
+        public string ItemId { get; set; }
+
+    }
+
+    [Table("WalMap")]
+    public class WalMap
+    {
+        public int ID { get; set; }
+        public string EbaySeller { get; set; }
+        public string EbayItemId { get; set; }
+        public string WalItemId { get; set; }
+        public string EbayUrl { get; set; }
+        public string Title { get; set; }
+        public int CategoryId { get; set; }
+        public long FeedbackScore { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsMultiVariationListing { get; set; }
+    }
+}
---
+++ wallib/Models/DataModelsDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallib.Models
+{
+    public class walDB : DbContext
+    {
+        static walDB()
+        {
+            //do not try to create a database 
+            Database.SetInitializer<walDB>(null);
+        }
+
+        public walDB()
+            : base("name=OPWContext")
+        {
+        }
+
+        public DbSet<WalItem> WalItems { get; set; }
+        public DbSet<WalMap> WalMapItems { get; set; }
+
+        public async Task ItemStore(WalItem item)
+        {
+            try
+            {
+                WalItems.Add(item);
+                await this.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void SellerResultSave(WalMap record)
+        {
+            this.WalMapItems.Add(record);
+            this.SaveChanges();
+        }
+
+        public void RemoveItemRecords(int categoryId)
+        {
+            Database.ExecuteSqlCommand("delete from WalItems where categoryId=" + categoryId.ToString());
+        }
+        public void RemoveMapRecords(int categoryId)
+        {
+            Database.ExecuteSqlCommand("delete from WalItems where categoryId=" + categoryId.ToString());
+        }
+    }
+}
